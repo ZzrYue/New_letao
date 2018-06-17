7b4233957c4ed6f3c15ef4fa578a0bcc03a095bf
wuhu <wuhu0723@126.com>
2018-06-17 14:52:25 +0800

    完成了首页制作

--- public/m/js/common.js
+++ public/m/js/common.js
@@ -1,11 +1,9 @@
-/**
- * Created by Administrator on 2017/12/19.
- */
+// 区域滚动初始化
 mui('.mui-scroll-wrapper').scroll({
-    deceleration: 0.0005, //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
-    indicators: false
+    deceleration: 0.0005,
+    indicators:false
 });
-
+// 轮播图初始化
 mui('.mui-slider').slider({
-    interval:0//自动轮播周期，若为0则不自动播放，默认为0；
+  interval:2000//自动轮播周期，若为0则不自动播放，默认为0；
 });
