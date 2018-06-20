4b788305890257a0ed4a596d529219efedbbbfe9
wuhu <wuhu0723@126.com>
2018-06-20 15:12:43 +0800

    完成登陆

--- public/m/js/login.js
+++ public/m/js/login.js
@@ -13,12 +13,28 @@ $(function(){
             },
             dataType:'json',
             success:function(result){
+                // {success:true}
                 console.log(result);
                 // 登陆成功
-                if(result.success == true){
+                if(result.success && result.success == true){
                     // 跳转到刚才那一页
+                    // 1.直接打开登陆页，没有其它的来源页》》首页
+                    // http://127.0.0.1:3000/m/login.html?redirectURL=http://127.0.0.1:3000/m/productDetail.html?proId=3
+                    // location.search:?redirectURL=http://127.0.0.1:3000/m/productDetail.html?proId=3
+                    if(location.search.indexOf("?redirectURL") == 0){
+                        // 跳转到来源页
+                        location.href = location.search.replace("?redirectURL=",'');
+                    }
+                    // 2.没有来源页，则跳转到来源页
+                    else{
+                        location.href="./letao_index.html";
+                    }
                 }
                 // 登陆失败
+                else{
+                    // mui提供的一个用于展示信息的函数--渐进渐出
+                    mui.toast(result.message);
+                }
             }
         });
     });
--- public/m/js/productDetail.js
+++ public/m/js/productDetail.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
 
     // 获取用户传入的产品id号
     // lt.getParamter(location.search) == {"proId":4}
@@ -7,37 +7,37 @@ $(function(){
 
     // 初始化下拉刷新组件
     mui.init({
-        pullRefresh : {
-          container:"#refreshContainer",//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
-          down : {
-            height:50,//可选,默认50.触发下拉刷新拖动距离,
-            auto: true,//可选,默认false.首次加载自动下拉刷新一次
-            contentdown : "下拉可以刷新",//可选，在下拉可刷新状态时，下拉刷新控件上显示的标题内容
-            contentover : "释放立即刷新",//可选，在释放可刷新状态时，下拉刷新控件上显示的标题内容
-            contentrefresh : "正在刷新...",//可选，正在刷新状态时，下拉刷新控件上显示的标题内容
-            callback :function(){
-                render();
-                mui('#refreshContainer').pullRefresh().endPulldownToRefresh();
-            } //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
-          }
+        pullRefresh: {
+            container: "#refreshContainer",//下拉刷新容器标识，querySelector能定位的css选择器均可，比如：id、.class等
+            down: {
+                height: 50,//可选,默认50.触发下拉刷新拖动距离,
+                auto: true,//可选,默认false.首次加载自动下拉刷新一次
+                contentdown: "下拉可以刷新",//可选，在下拉可刷新状态时，下拉刷新控件上显示的标题内容
+                contentover: "释放立即刷新",//可选，在释放可刷新状态时，下拉刷新控件上显示的标题内容
+                contentrefresh: "正在刷新...",//可选，正在刷新状态时，下拉刷新控件上显示的标题内容
+                callback: function () {
+                    render();
+                    mui('#refreshContainer').pullRefresh().endPulldownToRefresh();
+                } //必选，刷新函数，根据具体业务来编写，比如通过ajax从服务器获取新数据；
+            }
         }
-      });
+    });
 
     // 获取数据--封装为函数
-    
-    function render(){
+
+    function render() {
         $.ajax({
-            type:'get',
-            url:'/product/queryProductDetail',
-            data:{"id":id},
-            dataType:'json',
-            success:function(result){
-                var html = template("lr_proDetailTemp",result);
+            type: 'get',
+            url: '/product/queryProductDetail',
+            data: { "id": id },
+            dataType: 'json',
+            success: function (result) {
+                var html = template("lr_proDetailTemp", result);
                 $(".lt_proBox").html(html);
 
                 // 重新初始化轮播图组件
                 mui('.mui-slider').slider({
-                    interval:2000//自动轮播周期，若为0则不自动播放，默认为0；
+                    interval: 2000//自动轮播周期，若为0则不自动播放，默认为0；
                 });
                 // 重新初始化数字输入框
                 mui(".mui-numbox").numbox();
@@ -46,7 +46,7 @@ $(function(){
     }
 
     // 选择尺码
-    $(".lt_parent").on("tap",".lt_size > span",function(){
+    $(".lt_parent").on("tap", ".lt_size > span", function () {
         // 清除所有span的active样式
         $(this).siblings().removeClass("active");
         // 为当前span添加active样式
@@ -54,25 +54,30 @@ $(function(){
     })
 
     // 单击添加到购物车
-    $(".btn-addCart").on("tap",function(){
+    $(".btn-addCart").on("tap", function () {
         $.ajax({
-            type:'POST',
-            url:"/cart/addCart",
-            data:{
-                productId:id,
-                num:$(".mui-input-numbox").val(),
-                size:$(".lt_size > span.active").text()
+            type: 'POST',
+            url: "/cart/addCart",
+            data: {
+                productId: id,
+                num: $(".mui-input-numbox").val(),
+                size: $(".lt_size > span.active").text()
             },
-            dataType:'json',
-            success:function(result){
+            dataType: 'json',
+            success: function (result) {
                 // 判断是否登陆
                 // 如果登陆过，就弹出提示框，底部用户是继续逛逛，还是查看购物车
-                if(result.error != 400){
-
+                if (result.error != 400) {
+                    mui.confirm('添加成功，去购物车瞅瞅？', '温馨提示', ['是', '否'], function (e) {
+                        if (e.index == 0) {
+                            location.href = "./cart.html";
+                        }
+                    })
                 }
                 // 如果没有登陆，就直接跳转到登陆页
-                else{
-                    location.href = "./login.html";
+                else {
+                    // 跳转到登陆页，同时传入当前页面的url值，以便登陆成功之后能够回到当前页
+                    location.href = "./login.html?redirectURL=" + location.href;
                 }
             }
         });
